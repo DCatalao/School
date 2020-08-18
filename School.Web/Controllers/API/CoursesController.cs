6c8ea410f3d5ece668ea78e26fa182ad2affe337
Daniel Cardozo <catalao.daniel@gmail.com>
2020-08-18 15:03:56 +0100

    API Course Controller

---
+++ School.Web/Controllers/API/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using School.Web.Data;
+
+namespace School.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CoursesController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_courseRepository.GetAll());
+        }
+
+    }
+}
